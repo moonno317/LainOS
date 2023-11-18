402eaec01f8a58d7ddccccb6a74a1cf53ce482a6
moonno317 <139625145+moonno317@users.noreply.github.com>
2023-11-18 21:22:45 +0700

    Update kernel.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -9,26 +9,26 @@
 #include "GDTandIDT.h"
 #include "mm.h"
 #include "schedule.h"
+#include "fs.h"
+#include "initrd.h"
 
 void kernel_main(void) {
-    init_gdt();
-    init_idt();
-    terminal_initialize();
-    printf("Hi, I got GDT working\n");
-    init_dynamic_mem();
-    
-    size_t allocation_size = 1024;
-    void* allocated_memory;
-    int allocation_count = 0;
+  init_gdt();
+  init_idt();
+  terminal_initialize();
+  init_dynamic_mem();
+  size_t allocation_size = 1024;
+  void* allocated_memory;
+  int allocation_count = 0;
 
-    while ((allocated_memory = malloc(allocation_size)) != NULL) {
-        allocation_count++;
-        allocation_size *= 2; }
+  while ((allocated_memory = malloc(allocation_size)) != NULL) {
+    allocation_count++;
+    allocation_size *= 2; }
 
-    if (allocated_memory != NULL) {
-        *(int*)allocated_memory = 42; }
+  if (allocated_memory != NULL) {
+    *(int*)allocated_memory = 42; }
 
-   	free(allocated_memory);
-    while (1) {
-	schedule();
-        kbd(); } }
+  	free(allocated_memory);
+  while (1) {
+   schedule();
+   kbd(); } }
